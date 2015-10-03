f7c9cb201929bf8268745d7ca3bb4c0ff5465eb7
Wen Zhao <wenzhaorocks@gmail.com>
2015-10-03 19:20:10 +0800

    Refactor connect to another class

--- GoogleSync.java
+++ GoogleSync.java
@@ -1,166 +1,21 @@
 package memori;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.GregorianCalendar;
-import java.util.List;
-import java.util.Scanner;
-import java.util.StringTokenizer;
-
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.DateTime;
-import com.google.api.client.util.store.FileDataStoreFactory;
-import com.google.api.services.calendar.CalendarScopes;
-import com.google.api.services.calendar.model.Event;
-import com.google.api.services.calendar.model.EventDateTime;
-import com.google.api.services.calendar.model.Events;
 
 public class GoogleSync {
-	/** Application name. */
-	private static final String APPLICATION_NAME = "Memori";
 
-	/** Directory to store user credentials for this application. */
-	private static final java.io.File DATA_STORE_DIR = new java.io.File("GCALCredentials");
 
-	/** Global instance of the {@link FileDataStoreFactory}. */
-	private static FileDataStoreFactory DATA_STORE_FACTORY;
-
-	/** Global instance of the JSON factory. */
-	private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-
-	/** Global instance of the HTTP transport. */
-	private static HttpTransport HTTP_TRANSPORT;
-
-	/** Global instance of the scopes required by this quickstart. */
-	private static final List<String> SCOPES = Arrays.asList(CalendarScopes.CALENDAR);
-	
-	private static Scanner sc = new Scanner(System.in);
-
-	static {
-		try {
-			HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-			DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
-		} catch (Throwable t) {
-			t.printStackTrace();
-			System.exit(1);
-		}
-	}
-
-	/**
-	 * Creates an authorized Credential object.
-	 * 
-	 * @return an authorized Credential object.
-	 * @throws IOException
-	 */
-	public static Credential authorize() throws IOException {
-		// Load client secrets.
-		InputStream in = GoogleSync.class.getResourceAsStream("/client_secret.json");
-		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-		// Build flow and trigger user authorization request.
-		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY,
-				clientSecrets, SCOPES).setDataStoreFactory(DATA_STORE_FACTORY).setAccessType("offline").build();
-		Credential credential = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
-		System.out.println("Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
-		return credential;
-	}
-
-	/**
-	 * Build and return an authorized Calendar client service.
-	 * 
-	 * @return an authorized Calendar client service
-	 * @throws IOException
-	 */
-	public static com.google.api.services.calendar.Calendar getCalendarService() throws IOException {
-		Credential credential = authorize();
-		return new com.google.api.services.calendar.Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
-				.setApplicationName(APPLICATION_NAME).build();
-	}
-
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		// Build a new authorized API client service.
 		// Note: Do not confuse this class with the
 		// com.google.api.services.calendar.model.Calendar class.
-		com.google.api.services.calendar.Calendar service = getCalendarService();
-
-		// List the next 10 events from the primary calendar.
-		displayUpcoming(service);
-		// Create event
-		System.out.println(createEvent(service));
-	}
-
-	private static void displayUpcoming(com.google.api.services.calendar.Calendar service) throws IOException {
-		DateTime now = new DateTime(System.currentTimeMillis());
-		Events events = service.events().list("primary").setMaxResults(10).setTimeMin(now).setOrderBy("startTime")
-				.setSingleEvents(true).execute();
-		List<Event> items = events.getItems();
-		if (items.size() == 0) {
-			System.out.println("No upcoming events found.");
-		} else {
-			System.out.println("Upcoming events");
-			for (Event event : items) {
-				DateTime start = event.getStart().getDateTime();
-				if (start == null) {
-					start = event.getStart().getDate();
-				}
-				System.out.printf("%s (%s)\n", event.getSummary(), start);
-			}
-		}
-	}
-
-	private static String createEvent(com.google.api.services.calendar.Calendar service) {
-		System.out.println("Enter Name of event:");
-		String summary = sc.nextLine();
-		System.out.println("Enter location:");
-		String location = sc.nextLine();
-		System.out.println("Enter Description");
-		String description = sc.nextLine();
-		
-		System.out.println("Enter start Date:(dd/mm/yyyy)");
-		String rawDate = sc.nextLine();
-		StringTokenizer st = new StringTokenizer(rawDate,"/");
-		int day = Integer.parseInt(st.nextToken());
-		int month = Integer.parseInt(st.nextToken());
-		int year = Integer.parseInt(st.nextToken());
-		GregorianCalendar GC = new GregorianCalendar(year,month-1,day);
-		DateTime startDateTime = new DateTime(GC.getTimeInMillis());
-		System.out.println(startDateTime);
-		
-		System.out.println("Enter end Date:(dd/mm/yyyy)");
-		rawDate = sc.nextLine();
-		st = new StringTokenizer(rawDate,"/");
-		day = Integer.parseInt(st.nextToken());
-		month = Integer.parseInt(st.nextToken());
-		year = Integer.parseInt(st.nextToken());
-		
-		GC = new GregorianCalendar(year,month-1,day);
-		DateTime endDateTime = new DateTime(GC.getTimeInMillis());
-		System.out.println(endDateTime);
-		
-		Event event = new Event();
-		
-		event.setSummary(summary);
-		event.setLocation(location);
-		event.setDescription(description);
-		event.setStart(new EventDateTime().setDateTime(startDateTime));
-		event.setEnd(new EventDateTime().setDateTime(endDateTime));
-		String calendarId = "primary";
 		try {
-			event = service.events().insert(calendarId, event).execute();
+			com.google.api.services.calendar.Calendar service = GCalConnect.getCalendarService();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return event.getHtmlLink();
+
 	}
+
 }
