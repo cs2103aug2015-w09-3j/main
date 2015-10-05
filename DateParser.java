ad6b67430a46768ec2f0bafb80680793e69d6d5d
jayden shen <kudos456@hotmail.com>
2015-10-05 16:18:36 +0800

    minor changes to DateParser

--- DateParser.java
+++ DateParser.java
@@ -1,5 +1,7 @@
 package memori;
 
+
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -12,35 +14,31 @@ import com.joestelmach.natty.Parser;
 public class DateParser {
 	
 
-	public static void main(String[] args) {
-
-		Parser parser = new Parser();
-
-		Scanner sc = new Scanner(System.in);
-		while (true) {
-			String dateToParse = sc.nextLine();
-			List<Date> dateList = new ArrayList<Date>();
-			List<DateGroup> groups = parser.parse(dateToParse);
-			for (DateGroup group : groups) {
-				List<Date> dates = group.getDates();
-				int line = group.getLine();
-				int column = group.getPosition();
-				String matchingValue = group.getText();
-				String syntaxTree = group.getSyntaxTree().toStringTree();
-				Map parseMap = group.getParseLocations();
-				boolean isRecurreing = group.isRecurring();
-				Date recursUntil = group.getRecursUntil();
-
-				/*
-				 * if any Dates are present in current group then add them to
-				 * dateList
-				 */
-				if (group.getDates() != null) {
-					dateList.addAll(group.getDates());
-				}
-
-			}
-			System.out.println(dateList.get(0));
-		}
+	public DateParser(){
+		
 	}
+	public Date getDate(String dateToParse){
+		
+		Parser parser = new Parser();
+		List<Date> dateList = new ArrayList<Date>(); 
+		List<DateGroup> groups = parser.parse(dateToParse);
+		for(DateGroup group : groups){
+			List<Date> dates = group.getDates();
+			int line = group.getLine();
+			int column = group.getPosition();
+			String matchingValue = group.getText();
+			String syntaxTree = group.getSyntaxTree().toStringTree();
+			Map parseMap = group.getParseLocations();
+			boolean isRecurring = group.isRecurring();
+			Date recursUntil = group.getRecursUntil();
+		
+			if(group.getDates() != null){
+				dateList.addAll(group.getDates());
+			}
+		
+		}
+		return dateList.get(0);
+	
+	}
+
 }
