2862b5946aedba1e8fb3d334d896cacefa164a97
Wen Zhao <wenzhaorocks@gmail.com>
2015-10-06 20:20:44 +0800

    Removed unused libraries

--- GCalConnect.java
+++ GCalConnect.java
@@ -3,8 +3,6 @@ package memori;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.List;
 
--- MemoriEvent.java
+++ MemoriEvent.java
@@ -1,7 +1,5 @@
 package memori;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 import com.google.gson.Gson;
