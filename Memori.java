8378168e43029cc4f717978d09d2e730700e6fb6
Wen Zhao <wenzhaorocks@gmail.com>
2015-11-02 21:52:09 +0800

    UndoManager initial commit

--- Memori.java
+++ Memori.java
@@ -1,33 +1,32 @@
 package memori;
 
-
 public class Memori {
-	private MemoriUI  ui = new MemoriUI();
+	private MemoriUI ui = new MemoriUI();
 	private Storage st = Storage.getInstance();
 	private MemoriCalendar memoriCalendar;
-	private MemoriParser  memoriParser = new MemoriParser();
+	private MemoriParser memoriParser = new MemoriParser();
 	private MemoriSettings memoriSettings = MemoriSettings.getInstance();
 	private GoogleSync googleSync = new GoogleSync();
-	
-	private static String WELCOME_MSG = " ____    ____  ________  ____    ____   ___   _______     _____  " +
-										"\n|_   \\  /   _||_   __  ||_   \\  /   _|.'   `.|_   __ \\   |_   _|"+
-										"\n  |   \\/   |    | |_ \\_|  |   \\/   | /  .-.  \\ | |__) |    | |"+   
-										"\n  | |\\  /| |    |  _| _   | |\\  /| | | |   | | |  __ /     | |"+   
-										"\n  | |_\\/_| |_  _| |__/ | _| |_\\/_| |_\\  `-'  /_| |  \\ \\_  _| |_"+  
-										"\n|_____||_____||________||_____||_____|`.___.'|____| |___||_____| \n\n";
-	
-	private static final String COMMAND_PROMPT ="Command:";
-	
-	public void run(){
+
+	private static String WELCOME_MSG = " ____    ____  ________  ____    ____   ___   _______     _____  "
+			+ "\n|_   \\  /   _||_   __  ||_   \\  /   _|.'   `.|_   __ \\   |_   _|"
+			+ "\n  |   \\/   |    | |_ \\_|  |   \\/   | /  .-.  \\ | |__) |    | |"
+			+ "\n  | |\\  /| |    |  _| _   | |\\  /| | | |   | | |  __ /     | |"
+			+ "\n  | |_\\/_| |_  _| |__/ | _| |_\\/_| |_\\  `-'  /_| |  \\ \\_  _| |_"
+			+ "\n|_____||_____||________||_____||_____|`.___.'|____| |___||_____| \n\n";
+
+	private static final String COMMAND_PROMPT = "Command:";
+
+	public void run() {
 		setup();
 		executeCommands();
 	}
-	
-	public void executeCommands(){
-		while(true){
+
+	public void executeCommands() {
+		while (true) {
 			ui.displayToUser(COMMAND_PROMPT);
-			String userInput = ui.takeInput();		
-			if(userInput.equals("exit"))
+			String userInput = ui.takeInput();
+			if (userInput.equals("exit"))
 				System.exit(0);
 			MemoriCommand command = memoriParser.parse(userInput);
 			String ack = memoriCalendar.execute(command, googleSync);
@@ -36,13 +35,13 @@ public class Memori {
 			st.saveCalendar(memoriCalendar);
 		}
 	}
-	
+
 	public void setup() {
 		Thread memoriLockThread = new Thread(new MemoriLock());
-		//memoriLockThread.start();
+		// memoriLockThread.start();
 		memoriSettings = st.loadSettings();
 		memoriCalendar = st.loadCalendar();
-		if(memoriCalendar == null){
+		if (memoriCalendar == null) {
 			memoriCalendar = new MemoriCalendar();
 		}
 		memoriCalendar.initialize();
@@ -51,9 +50,24 @@ public class Memori {
 		ui.displayToUser(WELCOME_MSG);
 		ui.displayToUser(memoriCalendar.display());
 	}
-	
-	
-	public static void main(String[] args){
+
+	private boolean isCalendarCommand(MemoriCommand cmd) {
+		MemoriCommandType type = cmd.getType();
+		switch (type) {
+			case ADD:
+			case UPDATE:
+			case DELETE:
+			case SEARCH:
+			case SORT:
+			case COMPLETE:
+			case READ:
+				return true;
+			default:
+				return false;
+		}
+	}
+
+	public static void main(String[] args) {
 		Memori memori = new Memori();
 		memori.run();
 	}
