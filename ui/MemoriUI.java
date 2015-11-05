c37b5a0f2a93a9799063e36dadd69ae9c6285e85
Wen Zhao <wenzhaorocks@gmail.com>
2015-11-05 12:16:18 +0800

    Clear console function

--- ui/MemoriUI.java
+++ ui/MemoriUI.java
@@ -27,5 +27,11 @@ public class MemoriUI {
 	public String takeInput() {
 		return sc.nextLine();
 	}
+
+	public void clearConsole() {
+		for(int i=0;i< 50;i++){
+			System.out.println();
+		}
+	}
 	
 }
